63f4f98e6f258ac6cfd5d1cf7b47209b64a05f6e
miraj gansi <mirajgansi122@gmail.com>
2025-12-10 20:40:55 +0545

    bottom navigation created

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,14 +1,66 @@
+import 'package:click_shop/screens/bottom_screen/explore_screen.dart';
+import 'package:click_shop/screens/bottom_screen/favourite_screen.dart';
+import 'package:click_shop/screens/bottom_screen/cart_sreen.dart';
+import 'package:click_shop/screens/bottom_screen/home_screen.dart';
+import 'package:click_shop/screens/bottom_screen/profile_screen.dart';
 import 'package:flutter/material.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const HomeScreen(),
+    const ExploreScreen(),
+    const CartScreen(),
+    const FavouriteScreen(),
+    const ProfileScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text('Dashboard Screen', style: TextStyle(fontSize: 24)),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 30,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.house_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_rounded),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: 'Favourite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_3_outlined),
+            label: 'Account',
+          ),
+        ],
+        selectedItemColor: Color(0xFF53B175),
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
       ),
     );
   }
