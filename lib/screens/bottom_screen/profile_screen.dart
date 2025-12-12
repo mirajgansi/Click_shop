d6ff33a309db55aa228f1d02345645fd033f9ae6
miraj gansi <mirajgansi122@gmail.com>
2025-12-12 17:46:29 +0545

    name and email added

--- lib/screens/bottom_screen/profile_screen.dart
+++ lib/screens/bottom_screen/profile_screen.dart
@@ -6,7 +6,53 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
-      child: Center(child: Text("Welcome to the Profile Screen")),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  'assets/images/profile_picture.png',
+                ),
+              ),
+              const SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Row(
+                    children: [
+                      Text(
+                        'John Doe',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+
+                      Icon(Icons.edit, size: 16, color: Colors.grey),
+                    ],
+                  ),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Icon(Icons.email, size: 16, color: Colors.grey),
+
+                      Text(
+                        'JohnDoe@gmail.com',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/my_edit_button_widgets.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyEditButtonWidgets extends StatefulWidget {
+  const MyEditButtonWidgets({super.key});
+
+  @override
+  State<MyEditButtonWidgets> createState() => _MyEditButtonWidgetsState();
+}
+
+class _MyEditButtonWidgetsState extends State<MyEditButtonWidgets> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
